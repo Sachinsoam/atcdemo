c03098c46370ba6f3bc7df50bc4a63ffbcf85e3c
sachinsoam <you@example.com>
2024-11-14 16:13:47 +0530

    commit lbs change

--- terraform-eks/lbc.tf
+++ terraform-eks/lbc.tf
@@ -1,3 +1,4 @@
+# Define IAM policy document for AWS Load Balancer Controller (LBC)
 data "aws_iam_policy_document" "aws_lbc" {
   statement {
     effect = "Allow"
@@ -14,31 +15,35 @@ data "aws_iam_policy_document" "aws_lbc" {
   }
 }
 
+# Create IAM Role for AWS Load Balancer Controller
 resource "aws_iam_role" "aws_lbc" {
-  name               = "${aws_eks_cluster.eks.name}-aws-lbc"
+  name               = "${aws_eks_cluster.aws_eks.name}-aws-lbc"
   assume_role_policy = data.aws_iam_policy_document.aws_lbc.json
 }
 
+# Create IAM Policy for AWS Load Balancer Controller
 resource "aws_iam_policy" "aws_lbc" {
   policy = file("./AWSLoadBalancerController.json")
   name   = "AWSLoadBalancerController"
 }
 
+# Attach IAM Policy to Role
 resource "aws_iam_role_policy_attachment" "aws_lbc" {
   policy_arn = aws_iam_policy.aws_lbc.arn
   role       = aws_iam_role.aws_lbc.name
 }
 
+# Associate IAM Role with AWS Load Balancer Controller Pod Identity
 resource "aws_eks_pod_identity_association" "aws_lbc" {
-  cluster_name    = aws_eks_cluster.eks.name
+  cluster_name    = aws_eks_cluster.aws_eks.name
   namespace       = "kube-system"
   service_account = "aws-load-balancer-controller"
   role_arn        = aws_iam_role.aws_lbc.arn
 }
 
+# Helm Release for AWS Load Balancer Controller
 resource "helm_release" "aws_lbc" {
-  name = "aws-load-balancer-controller"
-
+  name       = "aws-load-balancer-controller"
   repository = "https://aws.github.io/eks-charts"
   chart      = "aws-load-balancer-controller"
   namespace  = "kube-system"
@@ -46,7 +51,7 @@ resource "helm_release" "aws_lbc" {
 
   set {
     name  = "clusterName"
-    value = aws_eks_cluster.eks.name
+    value = aws_eks_cluster.aws_eks.name
   }
 
   set {
@@ -54,5 +59,6 @@ resource "helm_release" "aws_lbc" {
     value = "aws-load-balancer-controller"
   }
 
+  # Define dependency on Cluster Autoscaler Helm release, if required
   depends_on = [helm_release.cluster_autoscaler]
 }
